7185a7c72181ada36518cf634492f749ac3f9914
NataliaCalvo <natalia.calvo@gmail.com>
2023-03-30 09:26:42 +0200

    Project-Setup

---
+++ src/main/java/com/Natalia/demo/IndyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Natalia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndyProjectApplication.class, args);
+	}
+
+}
