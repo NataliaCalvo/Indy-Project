624820523571020879fb9b55d89f458f8cadbef0
unknown <natalia.calvo@gmail.com>
2023-04-19 19:02:58 +0200

    Biography all placed

--- src/main/frontend/Indy/src/components/Biography.vue
+++ src/main/frontend/Indy/src/components/Biography.vue
@@ -7,17 +7,43 @@
         <img class="bioImgTitle" src="../assets/images/bioTitle.png" alt="Image of section's title: Biography"><br>
         <div class="imagesAndBioTextFirst">
             <img class="bioImg1" src="../assets/images/Feb 1990.jpg" alt="Image of Ismael as Indiana Jones in early 90's">
-            <p class="bioTextFirst">The year is 1981. An eleven-year old kid, sitting in a dark cinema room with two of his schoolmates, waits patiently for the film to start. He has been invited to watch this new movie – some Hollywood action flick – on the occasion of one of his friends’ birthday.
-            Little did he know at the time how deeply this event would influence his life. Two hours of sheer, mind-blowing entertainment that suddenly took – not just him, but millions of other people across the world – by storm.</p>
+            <article class="bioTextFirst">  The year is 1981. An eleven-year old kid, sitting in a dark cinema room with two of his schoolmates, waits patiently for the film to start. He has been invited to watch this new movie – some Hollywood action flick – on the occasion of one of his friends’ birthday.
+            Little did he know at the time how deeply this event would influence his life. Two hours of sheer, mind-blowing entertainment that suddenly took – not just him, but millions of other people across the world – by storm.</article>
+        </div>
         <div class="imagesAndBioTextSecond">
-            <p class="bioTextSecond">The film was Raiders of the Lost Ark…and that little kid was me. I still remember walking back home, dreamy and absent-minded, trying to digest the magnificent spectacle that I had just experienced.
+            <article class="bioTextSecond">The film was Raiders of the Lost Ark…and that little kid was me. I still remember walking back home, dreamy and absent-minded, trying to digest the magnificent spectacle that I had just experienced.
                 <br>
             Raiders was just the beginning – although, of all the films in the franchise, possibly the one that had the greatest impact on me. Indiana Jones and the Temple of Doom would follow only three years later…and then, of course, The Last Crusade. Oh boy, it was really cool to be a kid growing up in the eighties!
-            </p>
+            </article>
             <img class="bioImg2" src="../assets/images/imgBio2.png" alt="Image of Ismael as Indiana Jones in early 90's in his hometown of Gijon">
         </div>
-
-</div>
+        <div class="imagesAndBioTextThird">
+            <img class="bioImg3" src="../assets/images/imgBio3.png" alt="Image of Ismael as Indiana Jones nowadays">
+            <article class="bioTextThird">The appeal of the Indiana Jones character is unquestionable. It is simply iconic and somehow manages to reach all audiences – young and old. This, of course, has a lot to do with Harrison Ford’s charisma - but also with Indy’s determination and witty sense of humour. He is the courageous-yet-relatable adventurer who takes us to exotic lands and fires up our imagination. Who didn’t ever want to be Indy at some point in their lives?
+            <br>
+            No surprise it was named the greatest movie character of all time… after thousands of film fans cast their votes via the Empire magazine, back in 2015. Did you know that James Bond came only second?
+            <br>
+            Since Indiana Jones first hit the cinema screens back in 1981, fans all over the world have been fascinated not only by the storylines – but also by Indy’s iconic gear. I was one of them. 
+            <br>
+            </article>
+        </div>
+        <div class="imagesAndBioTextForth">
+            <article class="bioTextForth">Long before the times of social media and the Internet, I was trying to replicate the Indiana Jones look. And trust me…it wasn’t an easy thing to do. It was only with the passing of years, and through the use of new technologies, that I finally managed to find the right stuff. I tapped on the original sources, managed to locate the right vendors…always searching for screen-accuracy and authenticity. It was truly the search of a lifetime, but one that I have enjoyed and has made me many friends worldwide. 
+            <br>
+            What started as a kid’s dream, would later become a serious and enjoyable hobby – one that allows me, every now and then, to escape the dullness of a corporate job and get in touch with my inner child. That unique part in all of us that wants to have fun, to be free without limits.  
+            </article>
+            <img class="bioImg4" src="../assets/images/imgBio4.JPG" alt="Image of Indy's slash and a copy of the sanscrit text of The Temple of Doom">
+        </div>
+        <div class="imagesAndBioTextFifth">
+            <img class="bioImg5" src="../assets/images/imgBio5.JPG" alt="Image of Ismael as Indiana Jones nowadays">
+            <article class="bioTextFifth">One of the most memorable highlights of my cosplay activities was meeting Robert Watts (producer of the first three Indiana Jones and Star Wars films) in 2013. That year I had been asked to feature in a wonderful charity gala organised by Medicinema and Vue Cinemas in London – to celebrate the thirtieth anniversary of Temple of Doom.
+            <br>
+            Mr. Watts was there as a guest of honour. We spoke a lot, watched the restored print of the film together… and he even regaled me with an improvised commentary of the movie, on and off, during its whole running time!
+            <br>
+            Could this get any better? Well, yes…he also praised, and officially approved, my Indy gear!
+            <br>
+            </article>
+        </div>
 </body>
 </template>
 
@@ -26,6 +52,8 @@
 
 body{
     background-image: url('../assets/images/backgroundGradient.png');
+    background-repeat: no-repeat;
+    background-position: cover;
 }
 
 .bioImgTitle{
@@ -36,13 +64,14 @@ body{
 }
 
 .imagesAndBioTextFirst{
-    display: flex;
+    display:inline-flex;
     flex-direction: row;
 
 }
 
 .bioImg1{
     size:90%;
+    width: 40%;
     margin-top: 1%;
     border: 1%;
     border-style: solid;
@@ -55,12 +84,100 @@ body{
     margin-left: 5%;
     margin-top: 5%;
     margin-right: 2%;
+
 }
 
 .imagesAndBioTextSecond{
     display: flex;
     flex-direction: row;
+    margin-top: 10px;
 
 }
 
+.bioTextSecond {
+    font-family: 'SF Fedora Titles', sans-serif;
+    font-size: xx-large;
+    margin-left: 5%;
+    margin-top: 3%;
+    margin-right: 2%;
+}
+
+.bioImg2{
+    size:75%;
+    width: 40%;
+    margin-top: 1%;
+    border: 1%;
+    border-style: solid;
+    border-color: goldenrod;
+}
+
+.imagesAndBioTextThird{
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    margin-top: 4%;
+
+}
+
+.bioTextThird {
+    font-family: 'SF Fedora Titles', sans-serif;
+    font-size: xx-large;
+    margin-left: 5%;
+    margin-right: 2%;
+}
+
+.bioImg3{
+    size:50%;
+    margin-top: 1%;
+    border: 1%;
+    border-style: solid;
+    border-color: goldenrod;
+}
+.imagesAndBioTextForth{
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+
+}
+
+.bioTextForth {
+    font-family: 'SF Fedora Titles', sans-serif;
+    font-size: xx-large;
+    margin-left: 5%;
+    margin-top: 2%;
+    margin-right: 2%;
+}
+
+.bioImg4{
+    size:90%;
+    width: 40%;
+    margin-top: 3%;
+    border: 1%;
+    border-style: solid;
+    border-color: goldenrod;
+}
+
+.imagesAndBioTextFifth {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    margin-top: 4%;
+
+}
+
+.bioTextFifth {
+    font-family: 'SF Fedora Titles', sans-serif;
+    font-size: xx-large;
+    margin-left: 5%;
+    margin-top: 4%;
+    margin-right: 2%;
+}
+
+.bioImg5{
+    width: 20%;
+    margin-top: 1%;
+    border: 1%;
+    border-style: solid;
+    border-color: goldenrod;
+}
 </style>
